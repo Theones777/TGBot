e91f4542c8b241e2005fa9e801a6877fe8c7ef42
Theones777 <Zoolexi1995>
2022-06-01 12:09:07 +0300

    to aiogram - done

--- utils.py
+++ utils.py
@@ -79,3 +79,14 @@ def generate_markup(right_answer, wrong_answers):
     for item in list_items:  # Заполняем разметку перемешанными элементами
         markup.add(item)
     return markup
+
+
+# @bot.message_handler(commands=['test'])
+# def find_file_ids(message):
+#     for file in os.listdir('music/'):
+#         if file.split('.')[-1] == 'ogg':
+#             f = open('music/'+file, 'rb')
+#             msg = bot.send_voice(message.chat.id, f, None)
+#             # А теперь отправим вслед за файлом его file_id
+#             bot.send_message(message.chat.id, msg.voice.file_id, reply_to_message_id=msg.message_id)
+#         time.sleep(3)
